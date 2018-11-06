58e080dc0e157d2f7d38d5853579e46ce3da713e
Sven A. Schmidt <sas@finestructure.co>
2018-11-06 08:57:03 +0100

    Add sourcery template and git commit hook

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,32 @@
+// Generated using Sourcery 0.15.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
 import XCTest
 
-import FSHtmlTests
+// Add a marker protocol to allow for new TestCase subclasses to be
+// picked up automatically by adopting it.
+protocol LinuxTesting {}
+// Extend TestCase to conform
+extension TestCase: LinuxTesting {}
 
-var tests = [XCTestCaseEntry]()
-tests += FSHtmlTests.allTests()
-XCTMain(tests)
+@testable import FSHtml
+
+// swiftlint:disable trailing_comma
+extension FSHtmlTests {
+  static var allTests: [(String, (FSHtmlTests) -> () throws -> Void)] = [
+      ("testP", testP),
+      ("testAttr", testAttr),
+      ("testHtml", testHtml),
+  ]
+}
+extension XCTestCase {
+  static var allTests: [(String, (XCTestCase) -> () throws -> Void)] = [
+  ]
+}
+
+XCTMain([
+  testCase(FSHtmlTests.allTests),
+  testCase(XCTestCase.allTests),
+])
+// swiftlint:enable trailing_comma
---
+++ Tests/LinuxTesting.swift
@@ -0,0 +1,5 @@
+// Add a marker protocol to allow for new TestCase subclasses to be
+// picked up automatically by adopting it.
+protocol LinuxTesting {}
+// Extend XCTestCase to conform
+extension XCTestCase: LinuxTesting {}
